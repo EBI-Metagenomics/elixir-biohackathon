1fdfbf84a1788d9a03131eaa794d3865c508de2b
Miguel Boland <mdb@ebi.ac.uk>
2018-11-13 16:44:58 +0100

    Set minimum match 1% for regions, cleaned up files

--- classify_regions.py
+++ classify_regions.py
@@ -8,9 +8,11 @@ import json
 
 raw_f_regex = re.compile(
     "([A-z0-9.-]+)\s+-\s+(\w+)\s+(\w+)\s+(\w+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+([-+])\s+([-+])\s+(\d+)\s+(\d+[\.\d]*)\s+(\d+[\.\d]*)\s+(\d+[\.\d]*)\s+(.+)\s!\s+ -")
-RAW_DATA_FILE = 'SRR4341428_MERGED_FASTQ.cmsearch.all.tblout.deoverlapped_V1_V2'
 
 MIN_OVERLAP = 0.95
+
+MIN_MATCH_PROPORTION = 0.01
+
 regions = {
     'V1': [69, 92],
     'V2': [131, 239],
@@ -29,9 +31,6 @@ logging.basicConfig(level=logging.INFO)
 def calc_overlap(read, reg):
     read_s, read_f = read
     reg_s, reg_f = reg
-    if read_s > read_f:
-        print(read_s, read_f)
-        input()
     overlap = max(min(read_f, reg_f) - max(read_s, reg_s), 0)
     total = max(reg_f - reg_s, 0)
     try:
@@ -62,10 +61,12 @@ def normalise_results(region_matches):
         logging.debug(k, matches, ratio)
 
         if ratio > 0:
-            results[k] = round(ratio, 4)
-    results = sorted(results.items(), key=lambda x: x[1])
-    results.reverse()
-    return results
+            results[k] = {'match_proportion': round(ratio, 4), }
+    return filter_minimum_match_proportion(results)
+
+
+def filter_minimum_match_proportion(region_matches):
+    return {region: v for region, v in region_matches.items() if v['match_proportion'] >= MIN_MATCH_PROPORTION}
 
 
 def retrieve_regions(tblout_file, outfile):
@@ -75,14 +76,13 @@ def retrieve_regions(tblout_file, outfile):
         limits = list(map(int, read[4:6]))
         region_matches.extend(get_regions(limits))
     normalised_matches = normalise_results(region_matches)
-    print(json.dumps(normalised_matches))
     with open(outfile, 'w') as f:
         json.dump(normalised_matches, f)
 
 
 def parse_args(argv):
     parser = argparse.ArgumentParser(description='Tool to determine which regions were amplified in 16S data')
-    parser.add_argument('file', help='Overlapped tblout file')
+    parser.add_argument('--file', help='Overlapped tblout file')
     parser.add_argument('-o', '--output_file', default='amplified_regions.json')
     return parser.parse_args(argv)
 
