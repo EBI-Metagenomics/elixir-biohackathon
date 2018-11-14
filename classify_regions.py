e6bdc6d47c07cfcb547a1f782e00c0db12c7f4ff
Miguel Boland <mdb@ebi.ac.uk>
2018-11-14 17:05:08 +0100

    Added support for 18S data, added model creation files to repo

--- classify_regions.py
+++ classify_regions.py
@@ -13,7 +13,7 @@ MIN_OVERLAP = 0.95
 
 MIN_MATCH_PROPORTION = 0.01
 
-regions = {
+regions_16S = {
     'V1': [69, 92],
     'V2': [131, 239],
     'V3': [430, 487],
@@ -23,6 +23,17 @@ regions = {
     'V7': [1107, 1164],
     'V8': [1234, 1285],
     'V9': [1426, 1456]
+}
+
+regions_18S = {
+    'V1': [69, 105],
+    'V2': [135, 190],
+    'V3': [474, 545],
+    'V4': [627, 727],
+    'V5': [1059, 1102],
+    'V7': [1366, 1382],
+    'V8': [1528, 1608],
+    'V9': [1727, 1750]
 }
 
 logging.basicConfig(level=logging.INFO)
@@ -39,7 +50,7 @@ def calc_overlap(read, reg):
         return 0
 
 
-def get_regions(raw_sequence):
+def get_regions(raw_sequence, regions):
     return {region for region, limits in regions.items() if calc_overlap(raw_sequence, limits) >= MIN_OVERLAP}
 
 
@@ -49,7 +60,7 @@ def load_data(filename):
         return [l[0] for l in [raw_f_regex.findall(l) for l in f] if bool(l)]
 
 
-def normalise_results(region_matches):
+def normalise_results(region_matches, regions):
     count_matches = len(region_matches)
     results = {}
     for k in regions:
@@ -69,13 +80,16 @@ def filter_minimum_match_proportion(region_matches):
     return {region: v for region, v in region_matches.items() if v['match_proportion'] >= MIN_MATCH_PROPORTION}
 
 
-def retrieve_regions(tblout_file, outfile):
+def retrieve_regions(tblout_file, outfile, subunit_type):
+    regions = regions_16S if subunit_type == '16S' else regions_18S
+
     data = load_data(tblout_file)
     region_matches = []
     for read in data:
         limits = list(map(int, read[4:6]))
-        region_matches.extend(get_regions(limits))
-    normalised_matches = normalise_results(region_matches)
+        region_matches.extend(get_regions(limits, regions))
+    normalised_matches = normalise_results(region_matches, regions)
+    print(tblout_file, normalised_matches)
     with open(outfile, 'w') as f:
         json.dump(normalised_matches, f)
 
@@ -83,13 +97,14 @@ def retrieve_regions(tblout_file, outfile):
 def parse_args(argv):
     parser = argparse.ArgumentParser(description='Tool to determine which regions were amplified in 16S data')
     parser.add_argument('file', help='Overlapped tblout file')
+    parser.add_argument('subunit_type', choices=['16S', '18S'], help='rRNA subunit type (16S vs 18S)')
     parser.add_argument('-o', '--output_file', default='amplified_regions.json')
     return parser.parse_args(argv)
 
 
 def main(argv):
     args = parse_args(argv)
-    retrieve_regions(args.file, args.output_file)
+    retrieve_regions(args.file, args.output_file, args.subunit_type)
 
 
 if __name__ == '__main__':
